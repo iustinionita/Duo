b1978ee18e522e8ef39cf548e567d190b7ab1cf1
Iustin <ionita.iustin@gmail.com>
2021-03-02 19:28:20 +0000

    V2
    - Exp
    - Bug fixes
    - Widths

--- js.js
+++ js.js
@@ -1,4 +1,7 @@
 window.onload = function () {
+
+    // HERO PARRALAX EFFECT
+
     const hero = document.querySelector(".hero");
 
     hero.addEventListener("mousemove", (e) => {
@@ -7,12 +10,49 @@ window.onload = function () {
         hero.style.transition = "all .3s linear";
         hero.style.backgroundPositionX = offsetX + "px";
 
-    })
+    });
 
     //Revino la pozitia initiala ( optional )
     hero.addEventListener("mouseleave", (e) => {
         // hero.style.backgroundPositionX = 0;
-    })
+    });
+
+
+    //SWITCHER
+
+    const switcher = document.querySelector(".switcher");
+    const dot = document.querySelector(".dot");
+    const exp1 = document.querySelector("#exp-1");
+    const exp2 = document.querySelector("#exp-2");
+    switcher.addEventListener("click", (e) => {
+        switch (dot.style.left) {
+            case dot.style.left = "0px":
+                dot.style.left = "65px";
+                break;
+            case dot.style.left = "65px":
+                dot.style.left = "0px";
+                break;
+        }
+    });
 }
 
+
+// const switcher = document.querySelector(".switcher");
+//     const dot = document.querySelector(".dot");
+//     const exp1 = document.querySelector("#exp-1");
+//     const exp2 = document.querySelector("#exp-2");
+//     switcher.addEventListener("click", (e) => {
+//         switch (dot.style.left) {
+//             case dot.style.left = "0px":
+//                 dot.style.left = "65px";
+//                 exp1.style.color = "#b3b3b3";
+//                 exp2.style.color = "white";
+//                 break;
+//             case dot.style.left = "65px":
+//                 dot.style.left = "0px";
+//                 exp2.style.color = "#b3b3b3";
+//                 exp1.style.color = "white";
+//                 break;
+//         }
+//     });
 
