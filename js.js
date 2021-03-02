0767585c5332bff0e333b367e4d4104276b18491
Iustin <ionita.iustin@gmail.com>
2021-03-02 22:19:21 +0000

    V3
    - Scroll effect for Nav-Bar
    - Experience section:
    1. Switcher
    2. Cards -> interchangeble

--- js.js
+++ js.js
@@ -22,18 +22,53 @@ window.onload = function () {
 
     const switcher = document.querySelector(".switcher");
     const dot = document.querySelector(".dot");
-    const exp1 = document.querySelector("#exp-1");
-    const exp2 = document.querySelector("#exp-2");
+    const cardContent = document.querySelectorAll(".card-content");
+    const cardContent2 = document.querySelectorAll(".card-content2");
     switcher.addEventListener("click", (e) => {
         switch (dot.style.left) {
             case dot.style.left = "0px":
                 dot.style.left = "65px";
+
                 break;
             case dot.style.left = "65px":
                 dot.style.left = "0px";
                 break;
         }
     });
+
+    let switchPosition = 0;
+
+    switcher.addEventListener("click", (e) => {
+        if (switchPosition === 0) {
+            cardContent.forEach(element => {
+                element.setAttribute("style", "transform: translateY(120%);")
+            });
+            cardContent2.forEach(element => {
+                element.setAttribute("style", "transform: translateY(-100%);")
+            });
+            switchPosition = 1;
+        } else {
+            cardContent.forEach(element => {
+                element.setAttribute("style", "transform: translateY(0);")
+            });
+            cardContent2.forEach(element => {
+                element.setAttribute("style", "transform: translateY(30%);")
+            });
+            switchPosition = 0;
+        }
+
+    })
+
+    // NAV SHOW-UP ON SCROLL
+    const nav = document.querySelector('nav');
+
+    window.addEventListener("scroll", (e) => {
+        if (window.scrollY >> 200) {
+            nav.classList.add("nav-scroll");
+        } else if (window.scrollY << 200) {
+            nav.classList.remove("nav-scroll");
+        }
+    })
 }
 
 
